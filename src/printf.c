9ee98e0bb4085144bc0f8c838214aa7d6b042b8e
Daniel Engberg <daniel.engberg.lists@pyret.net>
2020-01-19 18:53:44 +0100

    Fix compilation on FreeBSD
    Add libutil header check for FreeBSD
    
    Signed-off-by: Daniel Engberg <daniel.engberg.lists@pyret.net>

--- src/printf.c
+++ src/printf.c
@@ -37,12 +37,16 @@
 #include <syslog.h>
 #include <limits.h>
 
-// Mac OS X has no pty.h, so use util.h instead
+// Include libutil
 #include "config.h"
 #ifdef HAVE_PTY_H
 	#include <pty.h>
-#else
+#endif
+#ifdef HAVE_UTIL_H
 	#include <util.h>
+#endif
+#ifdef HAVE_LIBUTIL_H
+	#include <libutil.h>
 #endif
 
 #include <sys/ioctl.h>
